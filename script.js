a53f7f1b4b3c56519ea3c550eea297b880a8c87d
mouhssina sola <mouhssina.sola2025@campus-eni.fr>
2025-04-17 16:53:22 +0200

    feat(script.js) premiere fonction js

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-document.getElementById('nomId').addEventListener('change', function (){
-    let valNomId = document.getElementById('nomId').value;
-// afficher un message qui indique qu'il faut mettre au minimum 3 caractères.
-    let valNomIdTab = valNomId.split("")
-    if (valNomIdTab.lengt < 2 ) {
-        document.getElementById('valNomIdTab').innerText = "veuillez renseigner 3 caractères minimum"
-    }
-    
+// //Pour afficher un message qui indique qu'il faut mettre au minimum 3 caractères.//
+
+
+// Mettre un ecouteur sur l'input pour voir si une valeur est rentré // Recupérer la valeur de l'input id
+document.getElementById('nomId').addEventListener('keyup', function() {
+    let idCheck = document.getElementById('nomId').value;
+    let msgIdusers = document.getElementById('msgId');
+    let pastilleValid = document.getElementById('imgCheckId')
+
+// Mettre une condition pour que si l'utilisateur commence à taper le message rouge s'affiche et s'efface lorsuque 3 caractères sont tapés 
+   
+if (idCheck.length > 2) {
+        msgIdusers.style.visibility = "hidden";
+        pastilleValid.src = "check.svg"
+    }else if (idCheck === '') {
+        msgIdusers.style.visibility = "hidden";
+        pastilleValid.style.visibility = "hidden"
+    }else{
+        msgIdusers.style.visibility = "visible";
+        pastilleValid.style.visibility = "visible"
+    } 
 })
 
-document.getElementById('')
+// pour afficher les pastilles vertes
+
+
+
+
+
+
+
